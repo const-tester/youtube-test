52b3506e131df6b64517ceda7d32517480863ece
const-tester <javier.cuervos11@gmail.com>
2026-06-29 20:16:56 +0200

    Fix members first tag on video

--- main.js
+++ main.js
@@ -78,7 +78,7 @@ const cssRemoveMembers = `
   ytd-two-column-browse-results-renderer #primary #contents ytd-item-section-renderer:has(#contents #action-button),
 
   /* Members first tag on video description */
-  #primary ytd-watch-metadata #description ytd-watch-info-text yt-formatted-string,
+  #primary ytd-watch-metadata #description ytd-watch-info-text yt-formatted-string:has(a),
   
   /* Videos on feed */
   ytd-rich-item-renderer:has(path[d^='M6 .5a5.5 5.5 0 100']),
